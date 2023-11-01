f7391aab4b822b418782ea26dfd7aa9948a7bf0b
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-11-02 00:42:48 +0530

    login mostly functionality done

--- Frontend/bmiapplication/src/Pages/login.jsx
+++ Frontend/bmiapplication/src/Pages/login.jsx
@@ -1,27 +1,39 @@
 import { Button, Container, FormControl, FormLabel, Input, Text } from "@chakra-ui/react"
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 export const Login = ()=>{
+    const [email,setEmail] = useState("");
+    const [password,setPassword]=useState("");
+
+    const handleLogin = (e)=>{
+e.preventDefault();
+let logiDetails ={
+    email,password
+}
+console.log(logiDetails)
+
+    }
     return <>
 <Container padding={'30px'}>
     <Text fontFamily={'cursive'} fontWeight={'bold'} color={'teal'}>Login here</Text> <br /><br />
     <Container border={'1px solid pink'} padding={'30px'} borderRadius={'5px'} >
-        <form action="">
+        <form onSubmit={handleLogin}>
 
 <FormControl isRequired>
     <FormLabel>Email</FormLabel>
-    <Input type="email" placeholder="Registered Email" />
+    <Input type="email" placeholder="Registered Email" onChange={(e)=>setEmail(e.target.value)} />
    
 </FormControl> <br />
 <FormControl isRequired>
     <FormLabel>Password</FormLabel>
     
-    <Input type="password" placeholder="password"   />
+    <Input type="password" placeholder="password" onChange={(e)=>setPassword(e.target.value)}  />
 </FormControl> <br /><br />
-<Button width={'100%'} colorScheme="pink">Login</Button>
+<Button width={'100%'} colorScheme="pink" type="submit">Login</Button>
 
         </form> <br /><br />
-        <Text fontFamily={'cursive'}>if you visit first Time, <span style={{color:"teal"}} fontWeight="800"><Link to="/signup">SignUp here!</Link></span></Text>
+        <Text fontFamily={'cursive'}>if you visit very first Time, <span style={{color:"teal"}} fontWeight="800"><Link to="/signup">SignUp here!</Link></span></Text>
     </Container> <br /><br />
 </Container>
     
