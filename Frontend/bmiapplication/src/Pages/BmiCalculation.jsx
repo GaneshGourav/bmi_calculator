89d2f77b204b88a7e6819ed0552c7865221d3c6e
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-11-01 21:41:10 +0530

    Basic Bmi calculation design done

--- Frontend/bmiapplication/src/Pages/BmiCalculation.jsx
+++ Frontend/bmiapplication/src/Pages/BmiCalculation.jsx
@@ -1,6 +1,25 @@
+import { Button, Container, FormControl, FormLabel, Input, Text } from "@chakra-ui/react"
+
 export const Bmicalculation = ()=>{
     return <>
+    <Container>
+<Text color={"teal"} fontFamily={'cursive'} fontWeight={"800"}>Calculate your Body Mass Index and stay healthy</Text>
+<Container>
+    <form action="">
+        <FormControl isRequired>
+            <FormLabel >Height</FormLabel>
+            <Input type="number" placeholder="height in feet"  />
+        </FormControl> <br />
+        <FormControl isRequired>
+            <FormLabel >Weight</FormLabel>
+            <Input type="number" placeholder="weight in Kgs"  />
+        </FormControl> <br />
+        <Button width="100%" colorScheme="pink">Calculate your BMI</Button> <br /><br />
+        <Button width="100%" colorScheme="yellow">Clear</Button>
+    </form>
+</Container>
 
-    <h1>Bmi Calculation</h1>
+</Container><br />
+    
     </>
 }
