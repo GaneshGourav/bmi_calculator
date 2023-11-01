127a338a967fe5e2709dd858450d2fff9eba5926
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-11-01 23:15:47 +0530

    Login frontend done

--- Frontend/bmiapplication/src/Pages/login.jsx
+++ Frontend/bmiapplication/src/Pages/login.jsx
@@ -1,20 +1,26 @@
-import { Container, FormControl, FormLabel, Input, Text } from "@chakra-ui/react"
+import { Button, Container, FormControl, FormLabel, Input, Text } from "@chakra-ui/react"
 
 export const Login = ()=>{
     return <>
-<Container>
-    <Text fontFamily={'cursive'} fontWeight={'bold'} color={'teal'}>Login here</Text>
-    <Container>
+<Container padding={'30px'}>
+    <Text fontFamily={'cursive'} fontWeight={'bold'} color={'teal'}>Login here</Text> <br /><br />
+    <Container border={'1px solid pink'} padding={'30px'} borderRadius={'5px'} >
         <form action="">
 
 <FormControl isRequired>
     <FormLabel>Email</FormLabel>
     <Input type="email" placeholder="Registered Email" />
-    <Input type="password" placeholder="password" />
-</FormControl>
+   
+</FormControl> <br />
+<FormControl isRequired>
+    <FormLabel>Password</FormLabel>
+    
+    <Input type="password" placeholder="password"   />
+</FormControl> <br /><br />
+<Button width={'100%'} colorScheme="pink">Login</Button>
 
         </form>
-    </Container>
+    </Container> <br /><br />
 </Container>
     
     </>
