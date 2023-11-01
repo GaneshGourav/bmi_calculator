3decfa3d0e5626b7dc5e1f6af56ce4c387fd7ad9
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-11-01 19:54:49 +0530

    Basic Home page

--- Frontend/bmiapplication/src/Components/Navbar.jsx
+++ Frontend/bmiapplication/src/Components/Navbar.jsx
@@ -40,6 +40,7 @@ export const Navbar = () => {
           </NavLink>
         ))}
       </div>
+      <hr />
     </>
   );
 };
--- Frontend/bmiapplication/src/Pages/Home.jsx
+++ Frontend/bmiapplication/src/Pages/Home.jsx
@@ -1,6 +1,14 @@
+import { Box, Container, Grid, GridItem, Text } from "@chakra-ui/react"
+
 export const Home = ()=>{
     return <>
 
-    <h1>Home</h1>
+<Container maxW={"10xl"} maxH={"max-content"}  height={"300px"} backgroundColor={"#372C2E"} alignItems={"center"} display={"flex"} justifyContent={'center'}>
+   
+<Text color={"teal"} fontFamily={'cursive'} fontWeight={'600'}>Welocme to Your BMI Health</Text>
+
+</Container>
+    
+  
     </>
 }
