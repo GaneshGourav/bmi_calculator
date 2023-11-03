8c600d77136c5ef21a9dc2f75c57ee3b2a96a5da
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-11-03 19:16:14 +0530

    login Reducer function added

--- Frontend/bmiapplication/src/Redux/Action.js
+++ Frontend/bmiapplication/src/Redux/Action.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
-import { SIGNUP_PENDING } from './ActionType'
+import { LOGIN_PENDING, SIGNUP_PENDING } from './ActionType'
 
 export const userSignup=(user)=>(dispatch)=>{
     dispatch({type:SIGNUP_PENDING})
     return axios.post('localhost:4000/users/signup',user)
+}
+
+export const userLogin=(data)=>(dispatch)=>{
+    dispatch({type:LOGIN_PENDING})
+    return axios.post('localhost:4000/users/login',data)
 }
