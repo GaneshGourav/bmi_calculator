6a4e7373cafd3b5e15f58de4f6bba3d5405cd84f
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-11-05 19:35:41 +0530

    istory router created

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 require("dotenv").config();
 const { connection } = require("./db");
 const { userRouter } = require("./router/userRouter");
+const {historyRouter} = require("./router/historyRouter")
 const cors = require('cors')
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/users", userRouter);
+app.use("/historys",historyRouter)
 
 app.get("/", async (req, res) => {
   try {
--- backend/router/historyRouter.js
+++ backend/router/historyRouter.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const {HistoryModel} = require('../model/historyModel')
 
-const historyRouter = express.Router;
+const historyRouter = express.Router();
 
 historyRouter.post("/data",async(req,res)=>{
-    try
+    const {data }= req.body;
+    try {
+        const newData = await HistoryModel({data});
+        newData.save();
+        res.status(200).json({"msg":"Data added to the server"});
+    } catch (error) {
+        res.status(500).json("Internal server Issue")
+    }
 })
 
+module.exports={historyRouter};
