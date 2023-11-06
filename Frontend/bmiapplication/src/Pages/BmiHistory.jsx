1a6a9ca5493f6110f8d28e4f40c4dab1ec9a11dc
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-11-06 23:14:41 +0530

    hstory table initialsd

--- Frontend/bmiapplication/src/Pages/BmiHistory.jsx
+++ Frontend/bmiapplication/src/Pages/BmiHistory.jsx
@@ -1,9 +1,35 @@
 import { Container } from "@chakra-ui/react"
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+  } from '@chakra-ui/react'
 
 export const BmiHistory = ()=>{
     return <>
 <Container>
-    
+<TableContainer>
+  <Table variant='striped' colorScheme='teal'>
+    <TableCaption>Body Mass Index measured history </TableCaption>
+    <Thead>
+      <Tr>
+        <Th>Sl.No.</Th>
+        <Th>BMI</Th>
+        <Th>Date</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+     
+    </Tbody>
+   
+  </Table>
+</TableContainer>
 </Container>
     
     </>
