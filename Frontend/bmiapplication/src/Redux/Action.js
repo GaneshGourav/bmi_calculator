26f14384495a00e18d67b775b95b872efd3f5cc7
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-11-07 22:36:22 +0530

    issue fixed

--- Frontend/bmiapplication/src/Redux/Action.js
+++ Frontend/bmiapplication/src/Redux/Action.js
@@ -4,13 +4,6 @@ import { BMI_PENDING, LOGIN_PENDING, SIGNUP_PENDING } from './ActionType'
 export const userSignup=(user)=>(dispatch)=>{
     dispatch({type:SIGNUP_PENDING})
     return axios.post('http://localhost:4000/users/signup',user)
-    // .then((res)=>{
-    //     dispatch({type:SIGNUP_SUCCESS,payload:res.data})
-    //     console.log(res.data)
-    //   })
-    //   .catch((err)=>{
-    //     dispatch({type:SIGNUP_ERROR})
-    //   })
       
 }
 
