b42e8c5fc599dda078f9b3b0d8f25c7ae1846859
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-12-01 22:22:44 +0530

    issue fixed

--- backend/model/historyModel.js
+++ backend/model/historyModel.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
-const HistorySchema = mongoose.Schema(
-  {
-   data:{type:String,required:true},
-   userID: String,
-   username: String,
-  },
-  {
-    versionKey: false,
-  }
-);
+// const HistorySchema = mongoose.Schema(
+//   {
+//    data:{type:String,required:true},
+//    userID: String,
+//    username: String,
+//   },
+//   {
+//     versionKey: false,
+//   }
+// );
 
 const HistoryModel = mongoose.model("history", HistorySchema);
 
