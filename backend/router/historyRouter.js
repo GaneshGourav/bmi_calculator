0b961b750a9911ad353e804ca07499b053ba81c8
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-12-01 22:22:17 +0530

    issue fixed

--- backend/router/historyRouter.js
+++ backend/router/historyRouter.js
@@ -3,16 +3,16 @@ const {HistoryModel} = require('../model/historyModel')
 
 const historyRouter = express.Router();
 
-// historyRouter.post("/add",async(req,res)=>{
-//     const data = req.body;
-//     try {
-//         const newData = new HistoryModel(data);
-//         newData.save();
-//         res.status(200).json({"msg":"Data added to the server",newData});
-//     } catch (error) {
-//         res.status(500).json("Internal server Issue")
-//     }
-// })
+historyRouter.post("/add",async(req,res)=>{
+    const data = req.body;
+    try {
+        const newData = new HistoryModel(data);
+        newData.save();
+        res.status(200).json({"msg":"Data added to the server",newData});
+    } catch (error) {
+        res.status(500).json("Internal server Issue")
+    }
+})
 
 historyRouter.get("/",async(req,res)=>{
     try {
